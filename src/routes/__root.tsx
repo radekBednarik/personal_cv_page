616b911201b8acc8d6fe6789e459efa8821f3363
radekBednarik <radek.bednarik@tesena.com>
2025-11-21 14:15:20 +0100

    fix: Remove double page view event firing

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,8 @@
 import { TanStackDevtools } from "@tanstack/react-devtools";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import { usePageViewTracking } from "@/lib/analytics";
 
 function RootComponent() {
-	// Track page views on all route changes
-	usePageViewTracking();
-
 	return (
 		<>
 			<Outlet />
